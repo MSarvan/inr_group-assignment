638aaca2c287e0474f52b497682b70ad7f599d24
Saravanan M <sarvan.m20@gmail.com>
2022-07-06 09:00:05 +0530

    Overlay effect implemented

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -5,81 +5,96 @@ import { Footer } from "./Footer";
 import img1 from "./Images/Img-1.png";
 import img2 from "./Images/Img-2.png";
 import img3 from "./Images/Img-3.png";
-import all from "./Images/Group 12028.png"
-import menu1 from "./Images/Group 12026.png"
-import menu2 from "./Images/Group 12029.png"
-import menu3 from "./Images/Group 12030.png"
-import menu4 from "./Images/Group 12031.png"
-import upArr from "./Images/Up-arr-icon.png"
+import all from "./Images/Group 12028.png";
+import menu1 from "./Images/Group 12026.png";
+import menu2 from "./Images/Group 12029.png";
+import menu3 from "./Images/Group 12030.png";
+import menu4 from "./Images/Group 12031.png";
+import upArr from "./Images/Up-arr-icon.png";
 
 export const Homepage = () => {
-
-    // function BasicUsage() {
-    //     var { isOpen, onOpen, onClose } = useDisclosure()
-    //     return (
-    //       <>
-    //         {/* <Button onClick={onOpen}>Open Modal</Button> */}
-      
-    //         <Modal isOpen={isOpen} onClose={onClose}>
-    //           <ModalOverlay />
-    //           <ModalContent>
-    //             <ModalHeader>Modal Title</ModalHeader>
-    //             <ModalCloseButton />
-    //             <ModalBody>
-    //               <Lorem count={2} />
-    //             </ModalBody>
-      
-    //             <ModalFooter>
-    //               <Button colorScheme='blue' mr={3} onClick={onClose}>
-    //                 Close
-    //               </Button>
-    //               <Button variant='ghost'>Secondary Action</Button>
-    //             </ModalFooter>
-    //           </ModalContent>
-    //         </Modal>
-    //       </>
-    //     )
-    //   }
-
-    const [data, setData] = useState([]);
-    const [showdata, setShowdata] = useState([]);
+  const [data, setData] = useState([]);
+  const [showdata, setShowdata] = useState([]);
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(5);
 
   useEffect(() => {
-    fetch(`http://localhost:7890/products`)
+    fetch(`http://localhost:7890/products?_limit=${size}&_page=${page}`)
       .then((res) => res.json())
-      .then((res) => console.log(res))
+      //   .then((res) => console.log(res))
       .then((res) => {
         setData(res);
         setShowdata(res);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
 
   return (
     <div>
       <Navbar />
       <div className="container">
-        <img src={img1} alt="1" />
-        <img src={img2} alt="2" />
-        <img src={img3} alt="3" />
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
       </div>
       <div className="btns">
-        <button><img src={all} alt="" className="all-btn"/></button>
-        <button><img src={menu1} alt="" /></button>
-        <button><img src={menu2} alt="" /></button>
-        <button><img src={menu3} alt="" /></button>
-        <button><img src={menu4} alt="" /></button>
-        <div className="uparr"><img src={upArr} alt=""/></div>
+        <button onClick={() => setSize(20)}>
+          <img src={all} alt="" className="all-btn" />
+        </button>
+        <button onClick={() => setPage(1)}>
+          <img src={menu1} alt="" />
+        </button>
+        <button onClick={() => setPage(2)}>
+          <img src={menu2} alt="" />
+        </button>
+        <button onClick={() => setPage(3)}>
+          <img src={menu3} alt="" />
+        </button>
+        <button onClick={() => setPage(4)}>
+          <img src={menu4} alt="" />
+        </button>
+        <div className="uparr">
+          <img src={upArr} alt="" />
+        </div>
       </div>
       <div className="showproducts">
-
+        {data.map((e) => (
+          <div className="products" key={e._id}>
+            <img src={e.proimage} alt="" className="pro-img" />
+            <div>
+              <h3>{e.name}</h3>
+              <p>
+                <span>By </span>
+                <span>
+                  <h4>{e.company}</h4>
+                </span>
+              </p>
+              <br />
+              <p>{e.description1}</p>
+              <p>
+                <span>{e.description2}</span>
+                <span>
+                  <h4>{e.read}</h4>
+                </span>
+              </p>
+              <br />
+              <img src={e.stars} alt="" />
+              <div className="price-details">
+                <p>
+                  <span>
+                    <h3>{e.price} </h3>
+                  </span>
+                  <span>{e.currency}</span>
+                </p>
+                <img src={e.thumbsup} alt="" />
+                <img src={e.message} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
+
       <Footer />
     </div>
   );
 };
-
-//5stars "https://i.ibb.co/hFhXB1f/Group-11996.png"
-//Product "https://i.ibb.co/QMX4rjr/Product.png"
-//Thumbsup "https://i.ibb.co/8nkZ8YL/Group-11995.png"
-//Message "https://i.ibb.co/T04z9rv/Group.png"
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/Navbar.css";
 import ealers from "./Images/Group 3.png";
 import smoke from "./Images/Group 2.png";
 import brands from "./Images/Group 5.png";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button
+} from "@chakra-ui/react";
+import { Loginpage } from "./Loginpage";
 
 export const Navbar = () => {
+
+  const OverlayOne = () => (
+    <ModalOverlay
+      bg="rgba(255, 255, 255, 0.4)"
+      backdropFilter="blur(5px) hue-rotate(90deg)"
+    />
+  );
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [overlay, setOverlay] = useState(<OverlayOne />);
+
   return (
     <div className="navbar-container">
       <div className="navbar-icons">
       <img src={ealers} alt="" />
       <img src={smoke} alt="" />
-      <img src={brands} alt="" />
+      <img src={brands} alt="" onClick={onOpen}/>
+      </div>
+      <div>
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          {overlay}
+          <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+              <Loginpage />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </div>
     </div>
+    
   );
 };
